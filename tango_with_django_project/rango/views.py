64c978ed3dfa32c763dedc96c0847dc71154bf49
Cavan-cloud <a584821373@163.com>
2021-07-30 06:00:58 +0100

    "chapter 10"

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,24 +5,37 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 
 def index(request):
+    #test
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['extra'] = 'From the model solution on GitHub'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+
+    visitor_cookie_handler(request)
 
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    # Spoiler: you don't need to pass a context dictionary here.
-    return render(request, 'rango/about.html')
+
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -47,7 +60,6 @@ def add_category(request):
 
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-
         if form.is_valid():
             form.save(commit=True)
             return redirect(reverse('rango:index'))
@@ -64,7 +76,6 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category = None
 
-    # You cannot add a page to a Category that does not exist... DM
     if category is None:
         return redirect(reverse('rango:index'))
 
@@ -82,7 +93,7 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)  # This could be better done; for the purposes of TwD, this is fine. DM.
+            print(form.errors)
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
@@ -114,16 +125,14 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    return render(request, 'rango/register.html',
-                  context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+    return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
+        user = authenticate(username=username, password=password)  # check if password is valid
 
         if user:
             if user.is_active:
@@ -138,6 +147,12 @@ def user_login(request):
         return render(request, 'rango/login.html')
 
 
+# def some_view(request):
+#     if not request.user.is_authenticated():
+#         return HttpResponse("You are logged in.")
+#     else:
+#         return HttpResponse("You are not logged in.")
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
@@ -147,3 +162,25 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
